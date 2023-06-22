4fa6f0150b527892466a55c01803b585d771c6ba
sonal-jk <sonal1024jk@gmail.com>
2023-06-22 21:02:12 +0530

    commit and push

--- source-code.cpp
+++ source-code.cpp
@@ -1,3 +1,6 @@
+/*BOOKSTORE MANAGEMENT SYSTEM*/
+
+
 #include <iostream>
 #include <windows.h>
 #include <C:\Program Files\MySQL\MySQL Server 8.0\include\mysql.h>
@@ -5,13 +8,72 @@
 #include <conio.h>
 using namespace std;
 int main(){
+    /*
     MYSQL* conn=NULL;
     conn = mysql_init(0);
     conn = mysql_real_connect(conn, "localhost", "root", "12345678", "rough", 0, NULL, 0);
 
     if(conn){
-        cout << "Connected" << endl;
+        std::cout << "Connected" << endl;
     } else {
-        cout << "Not connected" << endl;
+        std::cout << "Not connected" << endl;
+    }
+    int res=mysql_query(conn,"Insert into stud values(3)");
+    if (res!=0){
+        std::cout<<"error";
+    }
+    else{
+        std::cout<<"success";
+    }
+*/
+    /*START*/
+
+    std::cout<<"WELCOME TO THE BOOKSTORE!";
+
+    string ans="yes";
+    while (ans=="yes"){
+        int c;
+        std::cout<<" Please select an option from the given list by typing the number:\
+           \n 1. Books\n 2. Suppliers\n 3. Purchases\n 4. Employees\n 5. Members\n 6. Sales\n 7. Exit"<<"\n";
+        
+        std::cout<<"Select one option :";
+        std::cin>>c;
+        switch(c){
+            case 1:
+            std::cout<<"A";
+            break;
+            
+            case 2:
+            std::cout<<"B";
+            break;
+       
+            case 3:
+            std::cout<<"C";
+            break;
+
+            case 4:
+            std::cout<<"D";
+            break;
+            
+            case 5:
+            std::cout<<"E";
+            break;
+                   
+            case 6:
+            std::cout<<"F";
+            break;
+            
+            case 7:
+            exit(1);
+            break;
+            
+            default:
+            std::cout<<"Invalid Input";
+            break;
+       
+        }    
+    std::cout<<"\n"<<"Do you want to continue? Type yes or no :";
+    std::cin>>ans;
+
     }
 }
