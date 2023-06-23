81a39b8c62ebc38693338a25b4e55faa0c83797e
sonal-jk <sonal1024jk@gmail.com>
2023-06-23 19:32:50 +0530

    commit and push

--- source-code.cpp
+++ source-code.cpp
@@ -5,62 +5,19 @@
 #include "windows.h"
 #include <C:\Program Files\MySQL\MySQL Server 8.0\include\mysql.h>
 #include <conio.h>
-#include "classes.h"
+
 using namespace std;
+void book_menu();
+void sup_menu();
+void pur_menu();
+void emp_menu();
+void mem_menu();
+void sales_menu();
 
 /*CLASSES*/
 
 
-void menu(){
-     std::cout<<"WELCOME TO THE BOOKSTORE!";
 
-    string ans="yes";
-    while (ans=="yes"){
-        int c;
-        std::cout<<" Please select an option from the given list by typing the number:\
-           \n 1. Books\n 2. Suppliers\n 3. Purchases\n 4. Employees\n 5. Members\n 6. Sales\n 7. Exit"<<"\n";
-        
-        std::cout<<"Select one option :";
-        std::cin>>c;
-        switch(c){
-            case 1:
-            book_menu();
-            break;
-            
-            case 2:
-            std::cout<<"B";
-            break;
-       
-            case 3:
-            std::cout<<"C";
-            break;
-
-            case 4:
-            std::cout<<"D";
-            break;
-            
-            case 5:
-            std::cout<<"E";
-            break;
-                   
-            case 6:
-            std::cout<<"F";
-            break;
-            
-            case 7:
-            exit(1);
-            break;
-            
-            default:
-            std::cout<<"Invalid Input";
-            break;
-       
-        }    
-    std::cout<<"\n"<<"Do you want to continue? Type yes or no :";
-    std::cin>>ans;
-
-    }
-}
 
 int main(){
     /*
@@ -83,17 +40,66 @@ int main(){
 */
     /*START*/
 
-   menu();
-    return 0;
+   std::cout<<"WELCOME TO THE BOOKSTORE!";
+
+    string ans="yes";
+    while (ans=="yes"){
+        int c;
+        std::cout<<" Please select an option from the given list by typing the number:\
+           \n 1. Books\n 2. Suppliers\n 3. Purchases\n 4. Employees\n 5. Members\n 6. Sales\n 7. Exit"<<"\n";
+        
+        std::cout<<"Select one option :";
+        std::cin>>c;
+        std::cout<<"\n";
+        switch(c){
+            case 1:
+            book_menu();
+            break;
+            
+            case 2:
+            sup_menu();
+            break;
+       
+            case 3:
+            pur_menu();
+            break;
+
+            case 4:
+            emp_menu();
+            break;
+            
+            case 5:
+            mem_menu();
+            break;
+                   
+            case 6:
+            sales_menu();
+            break;
+            
+            case 7:
+            exit(1);
+            break;
+            
+            default:
+            std::cout<<"Invalid Input";
+            break;
+       
+        }    
+    std::cout<<"\n"<<"Do you want to continue? Type yes or no :";
+    std::cin>>ans;
+
+    }
+   return 0;
 }
 
 /*FUNCTIONS FOR MENUS OF DIFFERENT CATEGORIES*/
+
 void book_menu(){
   
     int c;
-    std::cout<<"Select one option\
-    \n 1. Add\n 2. Update Price\n 3.Search\n 4. Display All\n 5. Return to Main Menu\n";
-    std::cout<<"Your answer :";
+    std::cout<<"--BOOKS MENU--\
+    \n 1. Add\n 2. Update Price\n 3. Search\n 4. Display All\n 5. Return to Main Menu\n";
+    std::cout<<"Select an option :";
     std::cin>>c;
 
             switch(c){
@@ -123,4 +129,170 @@ void book_menu(){
         }    
     return;
 
+}
+
+void sup_menu(){
+  
+    int c;
+    std::cout<<"--SUPPLIERS MENU--\
+    \n 1. Add\n 2. Remove\n 3. Search\n 4. Return to Main Menu\n";
+    std::cout<<"Select an option :";
+    std::cin>>c;
+
+            switch(c){
+            case 1:
+            std::cout<<"A";
+            break;
+            
+            case 2:
+            std::cout<<"B";
+            break;
+       
+            case 3:
+            std::cout<<"C";
+            break;
+
+            case 4:
+            return;
+
+            default:
+            std::cout<<"Invalid Input";
+            break;
+       
+        }    
+    return;
+
+}
+
+void pur_menu(){
+  
+    int c;
+    std::cout<<"--PURCHASES MENU--\
+    \n 1. New Order\n 2. Cancel Order\n 3. View All\n 4. Received Order\n 5. Return to Main Menu\n";
+    std::cout<<"Select an option :";
+    std::cin>>c;
+
+            switch(c){
+            case 1:
+            std::cout<<"A";
+            break;
+            
+            case 2:
+            std::cout<<"B";
+            break;
+       
+            case 3:
+            std::cout<<"C";
+            break;
+
+            case 4:
+            std::cout<<"D";
+            break;
+            
+            case 5:
+            return;
+
+            default:
+            std::cout<<"Invalid Input";
+            break;
+       
+        }    
+    return;
+}
+
+void emp_menu(){
+  
+    int c;
+    std::cout<<"--EMPLOYEE MENU--\
+    \n 1. New Employee\n 2. Search Employee\n 3. Assign Manager\n 4. View All\n 5. Update Salary\n 6. Return to Main Menu\n";
+    std::cout<<"Select an option :";
+    std::cin>>c;
+
+            switch(c){
+            case 1:
+            std::cout<<"A";
+            break;
+            
+            case 2:
+            std::cout<<"B";
+            break;
+       
+            case 3:
+            std::cout<<"C";
+            break;
+
+            case 4:
+            std::cout<<"D";
+            break;
+
+            case 5:
+            std::cout<<"D";
+            break;
+            
+            case 6:
+            return;
+
+            default:
+            std::cout<<"Invalid Input";
+            break;
+       
+        }    
+    return;
+
+}
+
+void mem_menu(){
+  
+    int c;
+    std::cout<<"--MEMBERS MENU--\
+    \n 1. New Member\n 2. Search Member\n 3. Return to Main Menu\n";
+    std::cout<<"Select an option :";
+    std::cin>>c;
+
+            switch(c){
+            case 1:
+            std::cout<<"A";
+            break;
+            
+            case 2:
+            std::cout<<"B";
+            break;
+            
+            case 3:
+            return;
+
+            default:
+            std::cout<<"Invalid Input";
+            break;
+       
+        }    
+    return;
+}
+
+void sales_menu(){
+  
+    int c;
+    std::cout<<"--SALES MENU--\
+    \n 1. New Receipt\n 2. Total Sales\n 3. Return to Main Menu\n";
+    std::cout<<"Select an option :";
+    std::cin>>c;
+
+            switch(c){
+            case 1:
+            std::cout<<"A";
+            break;
+            
+            case 2:
+            std::cout<<"B";
+            break;
+       
+            case 3:
+            return;
+
+            default:
+            std::cout<<"Invalid Input";
+            break;
+       
+        }    
+    return;
 }
